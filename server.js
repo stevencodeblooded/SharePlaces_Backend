8e97cf1177b2c342b5de186b38a3fc4bf97e6240
stevencodeblooded <steven.ochieng432@gmail.com>
2023-11-09 11:42:37 +0300

    Reoved the console logs

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.use(cors())
 
 app.use('/assets', express.static('assets'))
 
-app.use((req, res, next) => {
-    console.log('Request URL:', req.originalUrl);
-    next();
-});
-
 app.use('/api/places', placesRoutes)
 app.use('/api/users', usersRoutes)
 
